d06c4be080ecb3fa91a349b9ac74c19f21bbbe00
sjabro <resting_wheeze@outlook.com>
2021-10-05 18:14:21 -0600

    Testing morph var

--- 1.0/aws/subnet/subnet.tf
+++ 1.0/aws/subnet/subnet.tf
@@ -9,7 +9,8 @@ variable "subnet_az" {
 }
 
 resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.vpc.id
+  # vpc_id            = aws_vpc.vpc.id
+  vpc_id = "<%=state.stateList.output.outputs.aws_vpc.id%>"
   cidr_block        = var.subnet_cidr
   availability_zone = var.subnet_az
 }
