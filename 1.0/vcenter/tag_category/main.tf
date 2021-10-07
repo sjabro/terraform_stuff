70f23cc49b54197aefb0d41d4b21afb8af2590f3
sjabro <resting_wheeze@outlook.com>
2021-10-07 12:45:39 -0600

    combined all into main

--- 1.0/vcenter/tag_category/main.tf
+++ 1.0/vcenter/tag_category/main.tf
@@ -1,7 +1,37 @@
+variable "category_name" {
+    type = string
+    default = "tf-test-category"
+}
+
+variable "category_types" {
+    type = list(string)
+
+    default = [
+         "VirtualMachine",
+         "Datastore" 
+        ]
+}
+
+variable "category_cardinality" {
+    type = string
+    default = "SINGLE"
+
+}
+
+variable "category_description" {
+    type = string
+    default = "Managed by Terraform"
+  
+}
+
 resource "vsphere_tag_category" "category" {
   name        = var.category_name
   description = var.category_description
   cardinality = var.category_cardinality
 
   associable_types = var.category_types
+}
+
+output "tag_category" {
+  value = vsphere_tag_category.category
 }
