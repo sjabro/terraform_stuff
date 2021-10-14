cf05486a106eb1eeee2eb1f1cc3679b2526b48ac
sjabro <resting_wheeze@outlook.com>
2021-10-14 16:47:42 -0600

    removed count

--- 1.0/aws/morpheus_classroom/vars.tf
+++ 1.0/aws/morpheus_classroom/vars.tf
@@ -10,11 +10,6 @@ variable "secret_key" {
     type = string
 }
 
-variable "env_count" {
-    type = number
-    default = 10
-}
-
 variable "vpc_root_cidr" {
     type = string
     default = "172.30.0.0/24"
