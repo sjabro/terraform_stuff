c5be2590b406b8622c4f582a49fd2e9a545a1042
sjabro <resting_wheeze@outlook.com>
2021-10-14 17:44:11 -0600

    removed the scripts

--- 1.0/aws/morpheus_classroom/install_morpheus.sh
+++
@@ -1,9 +0,0 @@
-#downloads and installs morpheus rpm
-
-MORPH_VER="<%=customOptions.morph_ver%>"
-
-cd /var/tmp
-
-wget https://downloads.morpheusdata.com/files/morpheus-appliance-${MORPH_VER}.el7.x86_64.rpm
-
-rpm -ivh morpheus-appliance-${MORPH_VER}.el7.x86_64.rpm
--- 1.0/aws/morpheus_classroom/morpheus.rb
+++
@@ -1,4 +0,0 @@
-## Url on which Morpheus will be reachable.
-## For more details on configuring appliance_url see:
-## https://www.morpheusdata.com/support/getting-started/advanced-configration
-appliance_url 'https://<%=server.externalIp%>'
