94fb70bf86d92cc28bf9bb96ccc28124d8dc8916
sjabro <resting_wheeze@outlook.com>
2021-10-18 11:36:07 -0600

    added non module

---
+++ 1.0/aws/morpheus_classroom_2/outputs.tf
@@ -0,0 +1,20 @@
+output "aws_vpc" {
+  value = aws_vpc.main
+}
+
+# output "security_group" {
+#     value = module.security-group
+# }
+
+# output "iam_user" {
+#     value = module.iam_user
+#     sensitive = true
+# }
+
+# output "ec2_instance" {
+#     value = module.ec2_instance
+# }
+
+# output "morpheus_instance" {
+#     value = module.morpheus_instance
+# }
