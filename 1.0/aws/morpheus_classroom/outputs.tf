bbd9926f86a231567451a7b485965da64c0e9123
sjabro <resting_wheeze@outlook.com>
2021-10-18 10:31:36 -0600

    Cleaned up outputs

--- 1.0/aws/morpheus_classroom/outputs.tf
+++ 1.0/aws/morpheus_classroom/outputs.tf
@@ -2,19 +2,19 @@ output "aws_vpc" {
   value = module.vpc
 }
 
-output "security_group" {
-    value = module.security-group
-}
+# output "security_group" {
+#     value = module.security-group
+# }
 
-output "iam_user" {
-    value = module.iam_user
-    sensitive = true
-}
+# output "iam_user" {
+#     value = module.iam_user
+#     sensitive = true
+# }
 
-output "ec2_instance" {
-    value = module.ec2_instance
-}
+# output "ec2_instance" {
+#     value = module.ec2_instance
+# }
 
-output "morpheus_instance" {
-    value = module.morpheus_instance
-}
+# output "morpheus_instance" {
+#     value = module.morpheus_instance
+# }
