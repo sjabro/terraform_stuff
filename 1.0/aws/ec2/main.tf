f7d475f54d5712b36960d7b2c21f49d351f48fef
sjabro <resting_wheeze@outlook.com>
2021-10-22 08:29:23 -0600

    updated ec2 instance

--- 1.0/aws/ec2/main.tf
+++ 1.0/aws/ec2/main.tf
@@ -2,10 +2,6 @@ resource "aws_instance" "ec2" {
   ami           = local.amis[var.region]
   instance_type = "t2.micro"
 
-  network_interface {
-    network_interface_id = aws_network_interface.net_if.id
-    device_index         = 0
-  }
 }
 
 output "aws_instance" {
