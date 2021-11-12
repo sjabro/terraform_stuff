d1249de25b7a753bfe7d672f25cb7e1ada703fbb
sjabro <resting_wheeze@outlook.com>
2021-11-11 21:47:18 -0700

    so many workarounds

--- 1.0/aws/morpheus_classroom/mor-adm/dev/locals.tf
+++ 1.0/aws/morpheus_classroom/mor-adm/dev/locals.tf
@@ -2,5 +2,5 @@ locals {
   # students = trim(["<%=customOptions.studentEmails%>"])
   # students = trim(local.students_given, "\"")
   # students = "${local.student_trim}"
-  student_list = {for s in local.students: index(var.students, s) => s}
+  student_list = {for s in var.students: index(var.students, s) => s}
 }
