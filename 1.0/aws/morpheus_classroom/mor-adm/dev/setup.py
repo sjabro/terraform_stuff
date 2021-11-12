e6f77cf76d5a59b847563db6b731194894068d83
sjabro <resting_wheeze@outlook.com>
2021-11-11 19:43:13 -0700

    uncommented

--- 1.0/aws/morpheus_classroom/mor-adm/dev/setup.py
+++ 1.0/aws/morpheus_classroom/mor-adm/dev/setup.py
@@ -28,45 +28,45 @@ class morphAppliance(object):
         self.access_token = access_token
         self.licence_key = license_key
     
-#     def applianceSetup(self):
-#         url = str("https://%s" % (self.app_ip))
-#         headers={'Content-Type': 'application/json',"Accept":"application/json"}
-#         body={ "applianceName": self.app_name, "applianceUrl": url, "accountName": self.account_name, "username": self.user_name, "password": self.password, "email": self.email, "firstName": self.first_name }
-#         b = json.dumps(body)
-#         response = requests.post(self.app_url, headers=headers, data=b, verify=False)
-#         data = response.json()
-#         print(data)   
+    def applianceSetup(self):
+        url = str("https://%s" % (self.app_ip))
+        headers={'Content-Type': 'application/json',"Accept":"application/json"}
+        body={ "applianceName": self.app_name, "applianceUrl": url, "accountName": self.account_name, "username": self.user_name, "password": self.password, "email": self.email, "firstName": self.first_name }
+        b = json.dumps(body)
+        response = requests.post(self.app_url, headers=headers, data=b, verify=False)
+        data = response.json()
+        print(data)   
     
-#     def getApiToken(self):
-#         url = str("https://%s/oauth/token?grant_type=password&scope=write&client_id=morph-api" % (self.app_ip))
-#         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-#         body = {"username": self.user_name, "password": self.password}
-#         response = requests.post(url, headers=headers, data=body, verify=False)
-#         data = response.json()
-#         return data['access_token']
+    def getApiToken(self):
+        url = str("https://%s/oauth/token?grant_type=password&scope=write&client_id=morph-api" % (self.app_ip))
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        body = {"username": self.user_name, "password": self.password}
+        response = requests.post(url, headers=headers, data=body, verify=False)
+        data = response.json()
+        return data['access_token']
         
-#     def applyLicense(self):
-#         url = str("https://%s/api/license" % (self.app_ip))
-#         headers={'Content-Type': 'application/json',"Authorization": "BEARER " + (self.access_token)}
-#         body = {"license": self.licence_key}
-#         b = json.dumps(body)
-#         response = requests.post(url, headers=headers, data=b, verify=False)
-#         data = response.json()
+    def applyLicense(self):
+        url = str("https://%s/api/license" % (self.app_ip))
+        headers={'Content-Type': 'application/json',"Authorization": "BEARER " + (self.access_token)}
+        body = {"license": self.licence_key}
+        b = json.dumps(body)
+        response = requests.post(url, headers=headers, data=b, verify=False)
+        data = response.json()
         
-#     def checkAppliancePing(self):
-#         url = str("https://%s/ping" % (self.app_ip))
-#         headers={'Content-Type': 'application/json',"Accept":"application/json"}
-#         response = requests.get(url, headers=headers, verify=False)
+    def checkAppliancePing(self):
+        url = str("https://%s/ping" % (self.app_ip))
+        headers={'Content-Type': 'application/json',"Accept":"application/json"}
+        response = requests.get(url, headers=headers, verify=False)
         
-# for c in instance:
-#     student_email = str(c['server']['name'].split('-')[0])
-#     ip = str(c['externalIp'])
+for c in instance:
+    student_email = str(c['server']['name'].split('-')[0])
+    ip = str(c['externalIp'])
     
-#     appliance = morphAppliance(app_name="Morpheus", app_ip=ip, account_name="Morpheus", user_name="admin", password="69F49!632b13e", email=student_email, first_name="admin", license_key=key)
+    appliance = morphAppliance(app_name="Morpheus", app_ip=ip, account_name="Morpheus", user_name="admin", password="69F49!632b13e", email=student_email, first_name="admin", license_key=key)
     
-#     print(appliance.account_name)
-#     print(appliance.app_name)
-#     print(appliance.app_ip)
-#     print(appliance.account_name)
-#     print(appliance.email)
-#     print(appliance.user_name)        
+    print(appliance.account_name)
+    print(appliance.app_name)
+    print(appliance.app_ip)
+    print(appliance.account_name)
+    print(appliance.email)
+    print(appliance.user_name)        
