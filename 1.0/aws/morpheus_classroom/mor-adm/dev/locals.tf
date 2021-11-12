02e8e374c12ea5e3aba5f75e15b4917eb2dc5a61
sjabro <resting_wheeze@outlook.com>
2021-11-12 09:59:07 -0700

    Added the delimiter to the split

--- 1.0/aws/morpheus_classroom/mor-adm/dev/locals.tf
+++ 1.0/aws/morpheus_classroom/mor-adm/dev/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  students = split("<%=customOptions.studentEmails%>")
+  students = split("<%=customOptions.studentEmails%>", ",")
   # students = trim(local.students_given, "\"")
   # students = "${local.student_trim}"
   student_list = {for s in local.students: index(local.students, s) => s}
