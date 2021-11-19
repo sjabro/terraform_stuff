3e1e0d952a356f049897f1a07a081fc9a05fcf7e
sjabro <resting_wheeze@outlook.com>
2021-11-19 16:52:46 -0700

    Trying to output spec

--- 1.0/aws/tests/tokenize/instance_rename.py
+++ 1.0/aws/tests/tokenize/instance_rename.py
@@ -2,9 +2,11 @@ import json
 
 config = morpheus['spec']    
 
+
+
 config['instance']['name'] = "newName"
 config['instance']['hostname'] = "newHostName"
 
 spec = "{ 'spec' : %s }" % (config)
-output = json.dumps(spec, indent=4)
-print(spec)
+# output = json.dumps(spec, indent=4)
+spec
