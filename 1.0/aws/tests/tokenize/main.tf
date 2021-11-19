47aa8672e8471c13b0f05f67db9d7d48542b5e03
sjabro <resting_wheeze@outlook.com>
2021-11-19 12:05:20 -0700

    UPdated tokenize

--- 1.0/aws/tests/tokenize/main.tf
+++ 1.0/aws/tests/tokenize/main.tf
@@ -11,9 +11,34 @@ provider "aws" {
 }
 
 locals {
-  output_1 = "<%=customOptions.fargateTokenize.tokenize('|')[0]%>"
+        App_Name = "<%=customOptions.fargateTokenize.tokenize('|')[0]%>"
+        Business_Group = "<%=customOptions.fargateTokenize.tokenize('|')[1]%>"
+        Cost_Code = "<%=customOptions.fargateTokenize.tokenize('|')[2]%>"
+        Business_Owner = "<%=customOptions.fargateTokenize.tokenize('|')[3]%>"
+        App_Code = "<%=customOptions.fargateTokenize.tokenize('|')[4]%>"
+        App_Owner = "<%=customOptions.fargateTokenize.tokenize('|')[5]%>"
 }
 
-output "output_1" {
+output "app_name" {
   value = local.output_1
+}
+
+output "business_group" {
+  value = local.Business_Group
+}
+
+output "cost_code" {
+  value = local.Cost_Code
+}
+
+output "business_owner" {
+  value = local.Business_Group
+}
+
+output "app_code" {
+  value = local.App_Code
+}
+
+output "app_owner" {
+  value = local.App_Owner
 }
