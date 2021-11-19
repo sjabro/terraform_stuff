385dd969d924c18ed23fd710e5cb95b1defe24a0
sjabro <resting_wheeze@outlook.com>
2021-11-19 15:27:19 -0700

    Updating

--- 1.0/aws/tests/tokenize/instance_rename.py
+++ 1.0/aws/tests/tokenize/instance_rename.py
@@ -3,6 +3,10 @@ import json
 specString = "<%=spec.encodeAsJson().toString()%>"
 configJson = json.loads(specString)
 
-print(configJson['instance']['name'])
-print(configJson['instance']['hostname'])
+# print(configJson['instance']['name'])
+# print(configJson['instance']['hostname'])
 
+configJson['instance']['name'] = "newName"
+configJson['instance']['name'] = "newHostName"
+
+print(configJson)
