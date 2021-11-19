caa18bd2c5feb1e075edc881396f045c6e18ebd0
sjabro <resting_wheeze@outlook.com>
2021-11-19 12:12:18 -0700

    testing nesting the local

--- 1.0/aws/tests/tokenize/main.tf
+++ 1.0/aws/tests/tokenize/main.tf
@@ -11,34 +11,40 @@ provider "aws" {
 }
 
 locals {
+    ApplicationTagData = {
         App_Name = "<%=customOptions.fargateTokenize.tokenize('|')[0]%>"
         Business_Group = "<%=customOptions.fargateTokenize.tokenize('|')[1]%>"
         Cost_Code = "<%=customOptions.fargateTokenize.tokenize('|')[2]%>"
         Business_Owner = "<%=customOptions.fargateTokenize.tokenize('|')[3]%>"
         App_Code = "<%=customOptions.fargateTokenize.tokenize('|')[4]%>"
         App_Owner = "<%=customOptions.fargateTokenize.tokenize('|')[5]%>"
+    }
 }
 
-output "app_name" {
-  value = local.App_Name
+output "ApplicationTagData" {
+  value = local.ApplicationTagData
 }
 
-output "business_group" {
-  value = local.Business_Group
-}
+# output "app_name" {
+#   value = local.App_Name
+# }
 
-output "cost_code" {
-  value = local.Cost_Code
-}
+# output "business_group" {
+#   value = local.Business_Group
+# }
 
-output "business_owner" {
-  value = local.Business_Group
-}
+# output "cost_code" {
+#   value = local.Cost_Code
+# }
 
-output "app_code" {
-  value = local.App_Code
-}
+# output "business_owner" {
+#   value = local.Business_Group
+# }
 
-output "app_owner" {
-  value = local.App_Owner
-}
+# output "app_code" {
+#   value = local.App_Code
+# }
+
+# output "app_owner" {
+#   value = local.App_Owner
+# }
