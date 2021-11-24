f9964e67ed8d88944f60454e29f47987aef6d0e7
sjabro <resting_wheeze@outlook.com>
2021-11-23 17:43:32 -0700

    setting static students

--- 1.0/aws/morpheus_classroom/mor-adm/dev/locals.tf
+++ 1.0/aws/morpheus_classroom/mor-adm/dev/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  students = split(",","<%=customOptions.studentEmails%>")
-  student_list = {for s in local.students: index(local.students, s) => s}
+  # students = split(",","<%=customOptions.studentEmails%>")
+  student_list = {for s in var.students: index(var.students, s) => s}
 }
