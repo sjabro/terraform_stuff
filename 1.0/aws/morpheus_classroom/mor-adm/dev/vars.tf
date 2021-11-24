cd61b4e464805954074a7aa8f210bdc67d72e80d
sjabro <resting_wheeze@outlook.com>
2021-11-24 12:10:53 -0700

    removed students

--- 1.0/aws/morpheus_classroom/mor-adm/dev/vars.tf
+++ 1.0/aws/morpheus_classroom/mor-adm/dev/vars.tf
@@ -10,11 +10,11 @@ variable "secret_key" {
     type = string
 }
 
-variable "students" {
-    type = list(string)
-    description = "Comma separate list of student emails. (With quotes)"
-    default = ["trainer","student1","student2","student3","student4","student5","student6","student7","student8","student9","student10"]
-}
+# variable "students" {
+#     type = list(string)
+#     description = "Comma separate list of student emails. (With quotes)"
+#     default = ["trainer","student1","student2","student3","student4","student5","student6","student7","student8","student9","student10"]
+# }
 
 variable "morph_version" {
     type = string
