d09aa28bf09fccd99358f22904cee5f2cd5a187a
sjabro <resting_wheeze@outlook.com>
2021-12-03 12:45:27 -0700

    took away the default

--- 1.0/For_B/tests/profile_parsing_test/main.tf
+++ 1.0/For_B/tests/profile_parsing_test/main.tf
@@ -106,10 +106,11 @@ output "map_of_bool" {
 
 variable "map_of_list_of_strings" {
   type = map(list(string))
-  default = {
-    "list1" : [ "string1-1","string1-2","string1-3" ]
-    "list2" : [ "string2-1","string2-2","string2-3"]
-  }
+
+  # default = {
+  #   "list1" : [ "string1-1","string1-2","string1-3" ]
+  #   "list2" : [ "string2-1","string2-2","string2-3"]
+  # }
 }
 
 output "map_of_list_of_string" {
