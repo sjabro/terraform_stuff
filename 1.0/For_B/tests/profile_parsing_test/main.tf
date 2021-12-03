09edf19c2cabc82c310321e69b234d06bab82c5f
sjabro <resting_wheeze@outlook.com>
2021-12-03 12:27:31 -0700

    updated var parsing

--- 1.0/For_B/tests/profile_parsing_test/main.tf
+++ 1.0/For_B/tests/profile_parsing_test/main.tf
@@ -10,17 +10,97 @@ terraform {
 provider "aws" {
 }
 
+##################################
+### BASIC VARS
+##################################
+
+variable "string" {
+  type = string
+  default = "This is a string"
+}
+
+output "string" {
+  value = var.string
+}
+variable "number" {
+  type = number
+  default = 42
+}
+
+output "number" {
+  value = var.number
+}
+
+variable "bool" {
+  type = bool
+  default = true
+}
+
+output "bool" {
+  value = var.bool
+}
+
+#####################################
+### TWO LAYER VARS
+#####################################
+
 variable "list_of_strings" {
   type = list(string)
   default = [ "string_1","string_2","string_3" ]
 }
 
+output "list_of_strings" {
+  value = var.list_of_strings
+}
+
+variable "map_of_strings" {
+  type = map(string)
+  default = {
+    "string_1" = "This is string 1"
+    "string_2" = "This is string 2"
+  }
+}
+
+output "map_of_strings" {
+  value = var.map_of_strings
+}
+
 variable "map_of_any" {
   type = map(any)
   default = {
-    "key_1" = "value_1",
+    "key_1" = "value_1"
     "key_2" = 12 
   }
+}
+
+output "map_of_any" {
+  value = var.map_of_any
+}
+
+variable "map_of_numbers" {
+  type = map(number)
+
+  default = {
+    "number_1" = 1
+    "number_2" = 2
+  }
+}
+
+output "map_of_numbers" {
+  value = var.map_of_numbers
+}
+
+variable "map_of_bool" {
+  type = map(bool)
+
+  default = {
+    "True" = true
+    "False" = false
+  }
+}
+
+output "map_of_bool" {
+  value = var.map_of_bool
 }
 
 variable "map_of_list_of_strings" {
@@ -31,14 +111,6 @@ variable "map_of_list_of_strings" {
   }
 }
 
-output "list_of_strings" {
-  value = var.list_of_strings
-}
-
-output "map_of_any" {
-  value = var.map_of_any
-}
-
-output "map_of_list_of_strings" {
-  value = var.map_of_list_of_strings
+output "map_of_list_of_string" {
+    value = var.map_of_list_of_strings 
 }
