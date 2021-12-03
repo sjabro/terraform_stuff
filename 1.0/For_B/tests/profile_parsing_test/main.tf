a88315902ea521b3f686a0382f67206da1c62314
sjabro <resting_wheeze@outlook.com>
2021-12-03 10:11:51 -0700

    Added map(list(string))

--- 1.0/For_B/tests/profile_parsing_test/main.tf
+++ 1.0/For_B/tests/profile_parsing_test/main.tf
@@ -12,10 +12,23 @@ provider "aws" {
 
 variable "list_of_strings" {
   type = list(string)
+  default = [ "string_1","string_2","string_3" ]
 }
 
 variable "map_of_any" {
   type = map(any)
+  default = {
+    "key_1" = "value_1",
+    "key_2" = 12 
+  }
+}
+
+variable "map_of_list_of_strings" {
+  type = map(list(string))
+  default = {
+    "list1" = [ "string1-1","string1-2","string1-3" ]
+    "list2" = [ "string2-1","string2-2","string2-3"]
+  }
 }
 
 output "list_of_strings" {
@@ -24,4 +37,8 @@ output "list_of_strings" {
 
 output "map_of_any" {
   value = var.map_of_any
+}
+
+output "map_of_list_of_strings" {
+  value = var.map_of_list_of_strings
 }
